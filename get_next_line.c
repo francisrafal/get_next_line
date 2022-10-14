0be0a514fd7b8330831329c7b54ecc813ad6017e
Francis Rafal <francis.rafal@gmail.com>
2022-10-14 11:26:57 +0200

    Read One Line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 10:18:29 by frafal            #+#    #+#             */
-/*   Updated: 2022/10/14 10:55:00 by frafal           ###   ########.fr       */
+/*   Updated: 2022/10/14 11:25:18 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,28 @@
 
 char	*get_next_line(int fd)
 {
-	void	*buf[BUFFER_SIZE];
+	char	buf[BUFFER_SIZE];
 	ssize_t	n;
+	size_t	i;
 	char	*line;
+	size_t	line_len;	
 
 	n = 0;
-	n = read(fd, buf + n, 1); 
-	line = (char *)malloc((n + 1) * sizeof(char));
+	n = read(fd, buf, BUFFER_SIZE); 
+	line_len = 0;
+	while (buf[line_len] != '\n' && n != 0 && line_len < BUFFER_SIZE)
+		line_len++;
+	
+	line = (char *)malloc((line_len + 1) * sizeof(char));
 	if (line == NULL)
 		return (NULL);
-	line[0] = ((char *)buf)[0];
-	line[1] = '\0';
+	i = 0;
+	while (i < line_len)
+	{
+		line[i] = buf[i];
+		i++;
+	} 
+	line[i] = '\n';
 	if (n > 0)
 		return (line); 
 	return (NULL);	
@@ -34,7 +45,7 @@ char	*get_next_line(int fd)
 
 int	main(void)
 {
-	char *line = get_next_line(0);
+	char	*line = get_next_line(0);
 	printf("%s", line);	
 	return (0);
 }
