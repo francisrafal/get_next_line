52be7de6205df5e3ee8d35be271a35734366d124
Francis Rafal <francis.rafal@gmail.com>
2022-10-14 10:56:18 +0200

    Read One Character From stdin

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,35 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 10:18:29 by frafal            #+#    #+#             */
-/*   Updated: 2022/10/14 10:18:31 by frafal           ###   ########.fr       */
+/*   Updated: 2022/10/14 10:55:00 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+char	*get_next_line(int fd)
+{
+	void	*buf[BUFFER_SIZE];
+	ssize_t	n;
+	char	*line;
+
+	n = 0;
+	n = read(fd, buf + n, 1); 
+	line = (char *)malloc((n + 1) * sizeof(char));
+	if (line == NULL)
+		return (NULL);
+	line[0] = ((char *)buf)[0];
+	line[1] = '\0';
+	if (n > 0)
+		return (line); 
+	return (NULL);	
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char *line = get_next_line(0);
+	printf("%s", line);	
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,15 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 10:20:05 by frafal            #+#    #+#             */
-/*   Updated: 2022/10/14 10:20:09 by frafal           ###   ########.fr       */
+/*   Updated: 2022/10/14 10:48:22 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
 
+char	*get_next_line(int fd);
+
+#endif
